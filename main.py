b490259e76f541f36b834f61c875082028301590
labinochka <labensckayam@yandex.ru>
2024-03-06 20:48:26 +0300

    tasks 3-4

--- main.py
+++ main.py
@@ -1,6 +1,31 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+# Вычисляем выбросы с помощью межквартильного размаха
+def outliers(column, dataset):
+    Q1 = dataset[column].quantile(0.25)
+    Q3 = dataset[column].quantile(0.75)
+
+    IQR = Q3 - Q1
+
+    lower_bound = Q1 - 1.5 * IQR
+    upper_bound = Q3 + 1.5 * IQR
+
+    outliers = dataset[(dataset[column] < lower_bound) | (dataset[column] > upper_bound)]
+    dataset = dataset[(dataset[column] > lower_bound) & (dataset[column] < upper_bound) & (dataset[column] > 0)]
+
+    print("Выбросы в столбце '{}':".format(column))
+    print(outliers[column])
+
+    return dataset
+
 
 data = pd.read_csv("hypothyroid.csv")
+
+# Столбцы
+columns = ["Age", "TSH", "T3", "T4", "T4U", "FTI"]
 
 print("---Основная информация о данных---")
 print()
@@ -12,27 +37,48 @@ print(data.dtypes)
 print()
 print("Количество пропущенных значений:")
 print(data.isnull().sum())
+print()
 
 # Удаляем строки с пропущенными значениями
-new_data = data.dropna()
+data = data.dropna()
 
-print()
+# Находим и удаляем выбросы
+for column in columns:
+    data = outliers(column, data)
+    print()
+
 print("---Основные статистические характеристики данных---")
 print()
 print("Средние значения:")
-print(new_data.mean(numeric_only=True))
+print(data.mean(numeric_only=True))
 print()
 print("Дисперсии:")
-print(new_data.var(numeric_only=True))
+print(data.var(numeric_only=True))
 print()
 print("Корреляции:")
-print(new_data.corr(numeric_only=True))
+print(data.corr(numeric_only=True))
 print()
 print("Минимумы:")
-print(new_data.min(numeric_only=True))
+print(data.min(numeric_only=True))
 print()
 print("Максимумы:")
-print(new_data.max(numeric_only=True))
+print(data.max(numeric_only=True))
 print()
 print("Квартили:")
-print(new_data.quantile([0.25, 0.5, 0.75], numeric_only=True))
+print(data.quantile([0.25, 0.5, 0.75], numeric_only=True))
+
+# Гистрограммы с графиком функции распределения
+for column in columns:
+    sns.histplot(data, x=column, kde=True)
+    plt.show()
+
+# Боксплоты
+for column in columns:
+    sns.boxplot(data, y=column)
+    plt.show()
+
+# Диаграммы рассеивания
+for i in range(len(columns)):
+    for j in range(i + 1, len(columns)):
+        sns.scatterplot(data=data, x=columns[i], y=columns[j])
+        plt.show()
